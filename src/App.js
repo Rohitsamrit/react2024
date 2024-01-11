8d63b4a79675099e034e258cf31514ad8fd747c2
rohitsamrit <rohitsamrit95@gmail.com>
2024-01-11 16:38:38 +0530

    starting phase

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="App-header">
+     <h1>Hello world</h1>
+     
+     </div>
+     <Demo/>
+    </div>
+  );
+}
+function Demo() {
+  return (
+    <div className="App">
+    <div className="App-header">
+     <h1>demo</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
